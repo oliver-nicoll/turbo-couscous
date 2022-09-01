f214cd59b5420ce053250630e254ca6786c2422f
Nicoll Oliver <nicolldon96@gmail.com>
2022-09-01 13:31:03 -0400

    child prop in card component

--- src/components/Programs.jsx
+++ src/components/Programs.jsx
@@ -1,11 +1,19 @@
 import {FaCrown} from 'react-icons/fa'
 import SectionHead from './SectionHead'
+import {programs} from '../data'
 
 const Programs = () => {
   return (
     <section className="programs">
         <div className="container programs__container">
             <SectionHead icon={<FaCrown/>} title='Programs'/>
+        </div>
+        <div className="programs__wrapper">
+            {
+                programs.map(({id, icon, title, info, path}) => {
+                    return <h4> Hi </h4>
+                })
+            }
         </div>
     </section>
   )
