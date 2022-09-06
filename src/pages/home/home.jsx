da3a9e4db82014e83346a651374f87cde326bf75
Nicoll Oliver <nicolldon96@gmail.com>
2022-09-05 21:31:51 -0400

    add component to home.jsx

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import FAQs from '../../components/FAQs'
 import MainHeader from '../../components/MainHeader'
 import Programs from '../../components/Programs'
 import Values from '../../components/Values'
@@ -10,6 +11,7 @@ const home = () => {
     <MainHeader /> 
     <Programs />
     <Values />
+    <FAQs/>
     </>
   )
 }
