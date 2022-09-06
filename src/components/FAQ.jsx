4b846d6de49f70f9ad97b2f2415efa974c9bdf06
Nicoll Oliver <nicolldon96@gmail.com>
2022-09-05 22:02:12 -0400

    add in icons

---
+++ src/components/FAQ.jsx
@@ -0,0 +1,16 @@
+import {AiOutlinePlus} from 'react-icons/ai'
+import {AiOutlineminus} from 'react-icons/ai'
+
+const FAQ = ({question, answer}) => {
+  return (
+    <article className="faq">
+        <div>
+            <h4>{question}</h4>
+            <button className="faq__icon"><AiOutlinePlus/></button>
+        </div>
+        <p>{answer}</p>
+    </article>
+  )
+}
+
+export default FAQ
