bf9caebda42a0a0d5f1eae56ed122d3f68dd9588
Nicoll Oliver <nicolldon96@gmail.com>
2022-09-06 10:35:38 -0400

    adding in useState to toggle btn

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,14 +1,23 @@
+import { useState } from 'react'
 import {AiOutlinePlus} from 'react-icons/ai'
-import {AiOutlineminus} from 'react-icons/ai'
+import {AiOutlineMinus} from 'react-icons/ai'
 
 const FAQ = ({question, answer}) => {
+
+    const [isAnswerShowing, setIsAnswerShowing] = useState(false)
+
+
   return (
-    <article className="faq">
+    <article className="faq" onClick={() => setIsAnswerShowing(prev => !prev)}>
         <div>
             <h4>{question}</h4>
-            <button className="faq__icon"><AiOutlinePlus/></button>
+            <button className="faq__icon">
+                {
+                    isAnswerShowing ? <AiOutlineMinus/> : <AiOutlinePlus/> 
+                }
+            </button>
         </div>
-        <p>{answer}</p>
+        {isAnswerShowing && <p>{answer}</p> }
     </article>
   )
 }
