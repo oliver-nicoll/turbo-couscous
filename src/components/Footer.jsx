c1425a889230d97f2b172487410fb778e803b483
Nicoll Oliver <nicolldon96@gmail.com>
2022-09-12 10:10:56 -0400

    fix footer words

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -43,32 +43,21 @@ const Footer = () => {
                 <Link to="/contact">Contact</Link>
             </article>
             <article>
-                <h4>Permalinks</h4>
-                <Link to="/about">About</Link>
-                <Link to="/plans">Plans</Link>
-                <Link to="/trainers">Trainers</Link>
-                <Link to="/gallery">Gallery</Link>
-                <Link to="/contact">Contact</Link>
+                <h4>Insights</h4>
+                <Link to="/s">Blog</Link>
+                <Link to="/s">Case Studies</Link>
+                <Link to="/s">Events</Link>
+                <Link to="/s">Communities</Link>
+                <Link to="/s">FAQs</Link>
             </article>
             <article>
-                <h4>Permalinks</h4>
-                <Link to="/about">About</Link>
-                <Link to="/plans">Plans</Link>
-                <Link to="/trainers">Trainers</Link>
-                <Link to="/gallery">Gallery</Link>
-                <Link to="/contact">Contact</Link>
-            </article>
-            <article>
-                <h4>Permalinks</h4>
-                <Link to="/about">About</Link>
-                <Link to="/plans">Plans</Link>
-                <Link to="/trainers">Trainers</Link>
-                <Link to="/gallery">Gallery</Link>
-                <Link to="/contact">Contact</Link>
+                <h4>Get In Touch</h4>
+                <Link to="/contact">Contact Us</Link>
+                <Link to="/s">Support</Link>
             </article>
         </div>
         <div className="footer__copyright">
-            <small>2022 - Oliver Crossfit.Co by Nicoll Oliver &copy;</small>
+            <small>2022 - Oliver Crossfit.Co by Nicoll Oliver &copy; ALL Rights Reserved</small>
         </div>
     </footer>
 
