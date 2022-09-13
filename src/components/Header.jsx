256f00a11c340d5b25c60f26151ad6903c88cc8d
Nicoll Oliver <nicolldon96@gmail.com>
2022-09-13 10:29:46 -0400

    header component add for other pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,14 @@ const Header = ({title, image, children}) => {
   return (
     <header className="header">
         <div className="header__container">
-            <img src={image} alt="Header Background Image" />
-        </div>
-        <div className="header__content">
-            <h2>{title}</h2>
-            <p>{children}</p>
-        </div>
+            <div className="header__container-bg">
+               <img src={image} alt="Header Background " />
+            </div>
+            <div className="header__content">
+                <h2>{title}</h2>
+                <p>{children}</p>
+            </div>
+        </div>     
     </header>
     )
 }
