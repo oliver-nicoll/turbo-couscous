ff621fadecf611ff9f5031d6d5fce1f6a7445608
Nicoll Oliver <nicolldon96@gmail.com>
2022-09-15 11:27:39 -0400

    not found component build

--- src/pages/notFound/notFound.jsx
+++ src/pages/notFound/notFound.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import './notFound.css'
+import { Link } from 'react-router-dom';
+import './notFound.css';
 
 const notFound = () => {
   return (
-    <div>notFound</div>
-  )
+    <section>
+      <div className="container notfound__container">
+        <h2>Page Not Found</h2>
+        <Link to="/" className='btn'>Go Back Home</Link>
+      </div>
+    </section>
+    )
 }
 
 export default notFound
