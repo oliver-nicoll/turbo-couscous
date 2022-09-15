4cde587770b5db4671f6f99bf8f80e958de257a1
Nicoll Oliver <nicolldon96@gmail.com>
2022-09-15 16:06:14 -0400

    final component

--- src/pages/trainers/trainers.jsx
+++ src/pages/trainers/trainers.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import Header from '../../components/Header'
+import HeaderImage from '../../images/avatar9.jpg'
+import {trainers} from '../../data'
+import {BsInstagram} from 'react-icons/bs'
+import {AiOutlineTwitter} from 'react-icons/ai'
+import {FaFacebook} from 'react-icons/fa'
+import { FaLinkedinIn } from 'react-icons/fa'
+import Trainer from '../../components/Trainer'
 import './trainers.css'
 
-const trainers = () => {
+const Trainers = () => {
   return (
-    <div>trainers</div>
-  )
+    <>
+      <Header title="Our Trainers" image={HeaderImage}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Repellat nihil temporibus maxime asperiores ipsam vel placeat ducimus ad sit nam ab illum impedit minima, eius praesentium consequatur hic perferendis veritatis?
+      </Header>
+      <section className='trainers'>
+        <div className="container trainers__container">
+          {
+            trainers.map(({id, image, name, job, socials}) => {
+              return <Trainer key={id} image={image} name={name} job={job} socials={
+                [
+                  {icon: <BsInstagram />, link: socials[0]},
+                  {icon: <AiOutlineTwitter />, link: socials[1]},
+                  {icon: <FaFacebook />, link: socials[2]},
+                  {icon: <FaLinkedinIn />, link: socials[3]},
+                ]
+                } />
+            })
+          }
+        </div>
+      </section>
+    </>
+    )
 }
 
-export default trainers
+export default Trainers
