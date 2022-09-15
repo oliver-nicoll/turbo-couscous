5587157ece8b8f77bbce5ed485554c595dfb20b3
Nicoll Oliver <nicolldon96@gmail.com>
2022-09-15 10:47:14 -0400

    map through image array

--- src/pages/gallery/gallery.jsx
+++ src/pages/gallery/gallery.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import Header from '../../components/Header'
+import HeaderImage from '../../images/avatar5.jpg'
+
 import './gallery.css'
 
 const gallery = () => {
+  const galleryLength = 9;
+  const images = []
+
+  for(let i = 1; i < galleryLength; i++) {
+    images.push(require(`../../images/avatar${i}.jpg`))
+  }
+
   return (
-    <div>gallery</div>
-  )
+    <>
+    <Header title="Our Gallery" image={HeaderImage}>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et ad doloribus officia 
+      corrupti cum at beatae. Animi odit vero placeat?
+    </Header>
+    <section className="gallery">
+      <div className="container gallery__container">
+        {
+          images.map((image, index) => {
+            return (
+            <article key={index}>
+              <img src={image} alt={`Gallery Image ${index + 1}`} />
+            </article>
+            )
+          })
+        }
+      </div>
+    </section>
+    </>
+    )
 }
 
 export default gallery
